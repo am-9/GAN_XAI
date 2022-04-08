7571f8b636e9bdc4b8ea5f89879fea429b331acb
Alaina Mahalanabis <amahalanabis@uwaterloo.ca>
2022-04-08 17:53:22 -0400

    wgan with penalty

--- src/experiment.py
+++ src/experiment.py
@@ -22,14 +22,21 @@ from utils.explanation_utils import explanation_hook, get_explanation, \
     explanation_hook_cifar, explanation_hook_lstm, get_explanation_IMV
 from torch.autograd import Variable
 from torch import nn
+from torch.optim import lr_scheduler
 import torch
 import time
 import ecg_dataset_pytorch
 from scipy.spatial.distance import euclidean
+from scipy.stats import wasserstein_distance, pearsonr
 from fastdtw import fastdtw
 import numpy as np
 import similaritymeasures
 import sys
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+import torch.distributions.kl as kl
+from models.discriminators import ECGLSTMDiscriminator, EcgCNNDiscriminator
+from models.generators import ECGLSTMGenerator, EcgCNNGenerator
 
 class Experiment:
     """ The class that contains the experiment details """
@@ -43,17 +50,35 @@ class Experiment:
         self.type = experimentType.value
         self.explainable = self.type["explainable"]
         self.explanationType = self.type["explanationType"]
-        self.generator = self.type["generator"]()
-        self.discriminator = self.type["discriminator"]()
-        self.g_optim = self.type["g_optim"](self.generator.parameters(), lr=self.type["glr"], betas=(0.5, 0.99))
-        self.d_optim = self.type["d_optim"](self.discriminator.parameters(), lr=self.type["dlr"], betas=(0.5, 0.99))
-        self.loss = self.type["loss"]
+
+        gen_model_dir = "/Users/alainamahalanabis/Documents/GAN_XAI/src/results/ecg/ECGCNN_Normal/generator.pt"
+        self.generator = EcgCNNGenerator()
+        self.generator.load_state_dict(torch.load(gen_model_dir), strict=False)
+        self.generator.eval()
+
+        disc_model_dir = "/Users/alainamahalanabis/Documents/GAN_XAI/src/results/ecg/ECGCNN_Normal/discriminator.pt"
+        self.discriminator = EcgCNNDiscriminator()
+        self.discriminator.load_state_dict(torch.load(disc_model_dir), strict=False)
+        self.discriminator.eval()
+
+        #self.generator = self.type["generator"]()
+        #self.discriminator = self.type["discriminator"]()
+        self.g_optim = self.type["g_optim"](self.generator.parameters(), betas=(0.0, 0.9))
+        self.d_optim = self.type["d_optim"](self.discriminator.parameters(), betas=(0.0, 0.9))
+        #self.g_schedule = lr_scheduler.ExponentialLR(self.g_optim, gamma=0.9)
+        #self.d_schedule = lr_scheduler.ExponentialLR(self.d_optim, gamma=0.9)
+        #self.loss = self.type["loss"]
         self.epochs = self.type["epochs"]
         self.cuda = False
         self.real_label = 0.9
         self.fake_label = 0.1
         self.samples = 16
         torch.backends.cudnn.benchmark = True
+        self.discriminator_iterations = 5
+        self.lambda_gp = 10
+        self.weight_clip = 0.01
+        self.alpha = self.type["alpha"]
+
 
     def run(self, logging_frequency=4) -> (list, list):
         """
@@ -69,6 +94,7 @@ class Experiment:
         start_time = time.time()
 
         #explanationSwitch = (self.epochs + 1) / 2 if self.epochs % 2 == 1 else self.epochs / 2
+        #explanationSwitch=self.epochs - 5
         explanationSwitch=0
 
         logger = Logger(self.name, self.type["dataset"])
@@ -99,11 +125,32 @@ class Experiment:
         # track losses
         G_losses = []
         D_losses = []
-        MMD = []
+
         DTW = []
-        FD = []
+        RMSE = []
+        WDIST = []
+        PEARSON = []
+        KL_DIV = []
 
         local_explainable = False
+
+        # mean = 0.0
+        #
+        # print ("calculating mean and var")
+        # for n_batch, images in enumerate(loader):
+        #     images = images['cardiac_cycle'].float()
+        #     batch_samples = images.size(0)
+        #     images = images.view(batch_samples, images.size(1), -1)
+        #     mean += images.mean(2).sum(0)
+        # mean = mean / len(loader.dataset)
+        #
+        # var = 0.0
+        # for n_batch, images in enumerate(loader):
+        #     images = images['cardiac_cycle'].float()
+        #     batch_samples = images.size(0)
+        #     images = images.view(batch_samples, images.size(1), -1)
+        #     var += ((images - mean.unsqueeze(1))**2).sum([0,2])
+        # std = torch.sqrt(var / (len(loader.dataset)*224*224))
 
         # Start training
         for epoch in range(1, self.epochs + 1):
@@ -122,8 +169,13 @@ class Experiment:
 
                 sys.stdout.flush()
                 real_batch = data['cardiac_cycle'].float()
-                #print ("batch number ", n_batch)
+                #print (real_batch[1])
+                #np.savetxt("./xAIoutput/F_beat_sinput.csv", real_batch[1], delimiter=',')
+                #exit(0)
+                print ("batch number ", n_batch)
                 labels = data['label']
+                #print(labels)
+
                 labels_class = torch.max(labels, 1)[1]
 
                 N = real_batch.size(0)
@@ -136,15 +188,17 @@ class Experiment:
                 else:
                     fake_data = self.generator(noise(N, self.cuda)).detach()
 
-
                 if self.cuda:
                     real_batch = real_batch.cuda()
                     fake_data = fake_data.cuda()
 
-                # Train D
-                d_error, d_pred_real, d_pred_fake = self._train_discriminator(real_data=real_batch, fake_data=fake_data,
-                                                                              local_explainable=local_explainable)
 
+                # Train D
+                for _ in range(self.discriminator_iterations):
+                    print("train d")
+                    fake_data = self.generator(noise(N, self.cuda)).detach()
+                    d_error, d_pred_real, d_pred_fake = self._train_discriminator(real_data=real_batch, fake_data=fake_data,
+                                                                              local_explainable=local_explainable)
                 # 2. Train Generator
                 # Generate fake data
 
@@ -153,13 +207,14 @@ class Experiment:
                 else:
                     fake_data = self.generator(noise(N, self.cuda))
 
-
                 if self.cuda:
                     fake_data = fake_data.cuda()
 
                 # Train G
                 g_error = self._train_generator(fake_data=fake_data, local_explainable=local_explainable,
                                                 trained_data=trained_data)
+
+                #sprint('done generator')
 
                 # Save Losses for plotting later
                 G_losses.append(g_error.item())
@@ -178,30 +233,51 @@ class Experiment:
 
                 #for each epoch calculate MMD and DTW
                 distance = 0
-                frechet = 0
+                rmse = 0
+                w_dist = 0
+                kl_div = 0
+                pearson = 0
+
                 if n_batch == len(loader)-1:
-                    #fake_data = (torch.sigmoid(fake_data)*2)-0.5
+
                     for i in range(N):
-                        with torch.no_grad():
-                            real = real_batch[i, :].numpy()
-                            fake = fake_data[i, :].numpy()
-                            if "CNN" in self.name:
-                                fake = np.interp(fake, (min(fake), max(fake)), (-0.4, 1.5))
-                            distance += fastdtw(real, fake, dist=euclidean, radius = 216)[0]
-                            frechet += similaritymeasures.frechet_dist(real, fake)
-                            if i == 120:
-                                print ("real array ", real)
-                                print ("fake array ", fake)
+                        for m in range(N):
+                            with torch.no_grad():
+                                #print("m ", m, "i ", i)
+                                real = real_batch[i, :].numpy()
+                                fake = fake_data[m, :].numpy()
+                                if "CNN" in self.name:
+                                    fake = np.interp(fake, (min(fake), max(fake)), (-0.4, 1.5))
+                                distance += fastdtw(real, fake, dist=euclidean, radius = 216)[0]
+                                rmse += np.sqrt((real-fake) ** 2).mean()
+                                w_dist += wasserstein_distance(real, fake)
+                                #print("wdist ", w_dist)
+                                #kl_div += np.sum(np.where(fake != 0, fake * np.log(fake / real), 0))
+                                pearson += pearsonr(real, fake)[0]
+                                #print("pearson ", pearson)
+                                #frechet += similaritymeasures.frechet_dist(real, fake)
+                        if i == 0:
+                            print ("real array ", real)
+                            print ("fake array ", fake)
 
-                    DTW.append(distance/N)
-                    FD.append(frechet/N)
-                    print ("dtw ", distance/N)
-                    print ("fd ", frechet/N)
+                    DTW.append(distance/(N**2))
+                    RMSE.append(rmse/(N**2))
+                    PEARSON.append(pearson/(N**2))
+                    WDIST.append(w_dist/(N**2))
+                    #KL_DIV.append(kl_div/(N**2))
+                    print ("dtw ", distance/(N**2))
+                    print ("rmse ", rmse/(N**2))
             #mmd_list.append(mmd_eval.item())
+            print("end of 1 epoch")
+            #self.g_schedule.step()
+            #self.d_schedule.step()
 
-        logger.save_models(generator=self.generator)
+        #logger.save_models(generator=self.generator)
+        #logger.save_discriminator(discriminator=self.discriminator)
         logger.save_errors(g_loss=G_losses, d_loss=D_losses)
-        logger.save_dtw_fd(DTW, FD, epoch, num_batches)
+        logger.save_dtw_rmse(DTW, RMSE, epoch, num_batches)
+        logger.save_pearson_wdist(PEARSON, WDIST, epoch, num_batches)
+        #logger.save_kldiv(KL_DIV, epoch, num_batches)
         timeTaken = time.time() - start_time
         test_images = self.generator(test_noise)
 
@@ -232,7 +308,6 @@ class Experiment:
         :param fake_data: tensor data created by generator
         :return: error of generator on this training step
         """
-
         N = fake_data.size(0)
 
         # Reset gradients
@@ -245,14 +320,15 @@ class Experiment:
             prediction = self.discriminator(fake_data).view(-1)
 
         if local_explainable and "LSTMIMV" not in self.name:
-            #print("local explanation true")
+            print("local explanation true")
             get_explanation(generated_data=fake_data, discriminator=self.discriminator, prediction=prediction,
                             XAItype=self.explanationType, cuda=self.cuda, trained_data=trained_data,
-                            data_type=self.type["dataset"])
+                            data_type=self.type["dataset"], alpha=self.alpha)
 
         # Calculate error and back-propagate
-        error = self.loss(prediction, values_target(size=(N,), value=self.real_label, cuda=self.cuda))
-
+        #error = self.loss(prediction, values_target(size=(N,), value=self.real_label, cuda=self.cuda))
+        print("prediction shape ", prediction.shape)
+        error = -torch.mean(prediction)
         error.backward()
 
         # clip gradients to avoid exploding gradient problem
@@ -284,11 +360,11 @@ class Experiment:
         if "IMV" in self.name:
             #print ("at discrminator IMV")
             prediction_real, alphas, betas = self.discriminator(real_data)
-            alphas = alphas.view(128, 216)
+            alphas = alphas.view(alphas.shape[0], alphas.shape[1])
             prediction_real = prediction_real.view(-1)
             #print ("preidction real ", prediction_real.shape)
             #print ("alphas size ", alphas.shape)
-            if local_explainable:
+            if local_explainable and self.explanationType=="IMV":
                 get_explanation_IMV(alphas)
                 #print("got alphas ", alphas.shape)
         else:
@@ -296,7 +372,7 @@ class Experiment:
             ##print (prediction_real)
 
         # Calculate error
-        error_real = self.loss(prediction_real, values_target(size=(N,), value=self.real_label, cuda=self.cuda))
+        #error_real = self.loss(prediction_real, values_target(size=(N,), value=self.real_label, cuda=self.cuda))
 
         # 1.2 Train on Fake Data
         if "IMV" in self.name:
@@ -306,10 +382,39 @@ class Experiment:
         prediction_fake = prediction_fake.view(-1)
 
         # Calculate error
-        error_fake = self.loss(prediction_fake, values_target(size=(N,), value=self.fake_label, cuda=self.cuda))
+        #error_fake = self.loss(prediction_fake, values_target(size=(N,), value=self.fake_label, cuda=self.cuda))
+
+
+        #Calculate gradient penalty
+        epsilon = torch.rand(N, 1, requires_grad = True).repeat(1, real_data.shape[1])
+        interpolated_data = real_data * epsilon + fake_data * (1-epsilon)
+        mixed_scores = self.discriminator(interpolated_data)
+        gradient = torch.autograd.grad(
+            inputs = interpolated_data,
+            outputs = mixed_scores,
+            grad_outputs = torch.ones_like(mixed_scores),
+            create_graph=True,
+            retain_graph=True
+        )[0]
+
+        gradient = gradient.view(gradient.shape[0], -1)
+
+        gradient_norm = gradient.norm(2, dim=1)
+        gp = torch.mean((gradient_norm -1)**2)
 
         # Sum up error and backpropagate
-        error = error_real + error_fake
+        #error = error_real + error_fake
+        error = (
+            -(torch.mean(prediction_real) - torch.mean(prediction_fake)) + self.lambda_gp*gp
+        )
+        error.requires_grad_()
+
+        # print ("error shape ", error.shape)
+        # print ("error ", error)
+        # print("real data shape ", real_data.shape)
+        # print("fake data shape ", fake_data.shape)
+        # print ("mean real data ", torch.mean(real_data))
+        # print ("mean real data ", torch.mean(fake_data))
 
         error.backward()
         #sprint ("D BACKWARD DONE ", error.shape)
@@ -317,5 +422,9 @@ class Experiment:
         # 1.3 Update weights with gradients
         self.d_optim.step()
 
+        # for p in self.discriminator.parameters():
+        #     p.data.clamp_(-self.weight_clip, self.weight_clip)
+        #Weight clipping for Wasserstein GAN
+
         # Return error and predictions for real and fake inputs
-        return (error_real + error_fake) / 2, prediction_real, prediction_fake
+        return error, prediction_real, prediction_fake
